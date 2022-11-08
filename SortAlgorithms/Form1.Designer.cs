1c6dbbd8c97df3d9826fef65864c1eb7af723d83
Arseny <20mineslend14@gmail.com>
2022-11-08 22:39:58 +0500

    создание элементов переделано на методы

--- SortAlgorithms/Form1.Designer.cs
+++ SortAlgorithms/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace SortAlgorithms
+﻿using System;
+using System.Windows.Forms;
+
+namespace SortAlgorithms
 {
     partial class Form1
     {
@@ -17,6 +20,7 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
@@ -36,11 +40,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.FillButton = new System.Windows.Forms.Button();
             this.FillTextBox = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();          
+            this.panel3 = new System.Windows.Forms.Panel();
             this.TimeLbl = new System.Windows.Forms.Label();
             this.CompareLbl = new System.Windows.Forms.Label();
-            this.SwopLbl = new System.Windows.Forms.Label();          
-            this.SelectionSortBtn = new System.Windows.Forms.Button();          
+            this.SwopLbl = new System.Windows.Forms.Label();
+            this.SelectionSortBtn = new System.Windows.Forms.Button();
             this.QuickSortBtn = new System.Windows.Forms.Button();
             this.ClearBtn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
@@ -52,153 +56,100 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.AddButton);
             this.panel1.Controls.Add(this.AddTextBox);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(333, 61);
-            this.panel1.TabIndex = 0;
+            
+            CreatePanel(panel:panel1, panelIndex:0, name:"panel1", x:0, y:0, width:333, height:61);
+            
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Добавить число";
+            CreateLbl(label: this.label1, name: "label1", text:"Добавить число", lablIndex:2, x:12, y:9);
+            
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(246, 24);
-            this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(75, 23);
-            this.AddButton.TabIndex = 1;
-            this.AddButton.Text = "Добавить";
-            this.AddButton.UseVisualStyleBackColor = true;
-            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            CreateBtn(x: 246, y:24, btnName:"AddButton", tabIndex:1, btnText:"Добавить", handler:this.AddButton_Click, btn:this.AddButton);
+            
             // 
             // ClearButton
             // 
-            this.ClearBtn.Location = new System.Drawing.Point(246, 57);
-            this.ClearBtn.Name = "ClearButton";
-            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
-            this.ClearBtn.TabIndex = 1;
-            this.ClearBtn.Text = "Очистить";
-            this.ClearBtn.UseVisualStyleBackColor = true;
-            this.ClearBtn.Click += new System.EventHandler(this.Clear);
+            CreateBtn(x: 246, y:57, btnName:"ClearButton", tabIndex:1, btnText:"Очистить", handler:this.Clear, btn:this.ClearBtn);
+            
             // 
             // AddTextBox
             // 
-            this.AddTextBox.Location = new System.Drawing.Point(15, 25);
-            this.AddTextBox.Name = "AddTextBox";
-            this.AddTextBox.Size = new System.Drawing.Size(228, 20);
-            this.AddTextBox.TabIndex = 0;
+            CreateTextBox(textBox:this.AddTextBox, name:"AddTextBox");
+            
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.FillButton);
             this.panel2.Controls.Add(this.FillTextBox);
-            this.panel2.Location = new System.Drawing.Point(0, 67);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(333, 61);
-            this.panel2.TabIndex = 3;
+            
+            CreatePanel(panel:this.panel2, panelIndex:3, name:"panel2", x:0, y:67, width:333, height:61);
+            
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(213, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Заполнить массив случайными числами";
+            CreateLbl(label: this.label3, name: "label3", text:"Заполнить случайными числами", lablIndex:2, x:12, y:9);
+            
             // 
             // FillButton
             // 
-            this.FillButton.Location = new System.Drawing.Point(246, 24);
-            this.FillButton.Name = "FillButton";
-            this.FillButton.Size = new System.Drawing.Size(75, 23);
-            this.FillButton.TabIndex = 1;
-            this.FillButton.Text = "Заполнить";
-            this.FillButton.UseVisualStyleBackColor = true;
-            this.FillButton.Click += new System.EventHandler(this.FillButton_Click);
+            CreateBtn(x: 246, y:24, btnName:"FillButton", tabIndex:1, btnText:"Заполнить", handler:this.FillButton_Click, btn:this.FillButton);
+            
             // 
             // FillTextBox
             // 
-            this.FillTextBox.Location = new System.Drawing.Point(15, 25);
-            this.FillTextBox.Name = "FillTextBox";
-            this.FillTextBox.Size = new System.Drawing.Size(228, 20);
-            this.FillTextBox.TabIndex = 0;
+            CreateTextBox(textBox:this.FillTextBox, name:"FillTextBox");
+            
             // 
             // panel3
             // 
-            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel3.Location = new System.Drawing.Point(339, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(694, 128);
-            this.panel3.TabIndex = 4;
+            this.panel3.Anchor =
+                ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top |
+                                                        System.Windows.Forms.AnchorStyles.Left)
+                                                       | System.Windows.Forms.AnchorStyles.Right)));
+            
+            CreatePanel(panel:this.panel3, panelIndex:4, name:"panel3", x:339, y:0, width:694, height:128);
             
             // 
             // TimeLbl
             // 
-            this.TimeLbl.AutoSize = true;
-            this.TimeLbl.Location = new System.Drawing.Point(9, 160);
-            this.TimeLbl.Name = "TimeLbl";
-            this.TimeLbl.Size = new System.Drawing.Size(43, 13);
-            this.TimeLbl.TabIndex = 6;
-            this.TimeLbl.Text = "Время:";
+            CreateLbl(label: this.TimeLbl, name: "TimeLbl", text:"Время:", lablIndex:6, x:9, y:160);
+            
             // 
             // CompareLbl
             // 
-            this.CompareLbl.AutoSize = true;
-            this.CompareLbl.Location = new System.Drawing.Point(9, 182);
-            this.CompareLbl.Name = "CompareLbl";
-            this.CompareLbl.Size = new System.Drawing.Size(130, 13);
-            this.CompareLbl.TabIndex = 7;
-            this.CompareLbl.Text = "Количество Сравнений: ";
+            CreateLbl(label: this.CompareLbl, name: "CompareLbl", text:"Количество Сравнений: ", lablIndex:7, x:9, y:182);
+            
             // 
             // SwopLbl
             // 
-            this.SwopLbl.AutoSize = true;
-            this.SwopLbl.Location = new System.Drawing.Point(9, 204);
-            this.SwopLbl.Name = "SwopLbl";
-            this.SwopLbl.Size = new System.Drawing.Size(119, 13);
-            this.SwopLbl.TabIndex = 8;
-            this.SwopLbl.Text = "Количество обменов: ";
-           
+            CreateLbl(label: this.SwopLbl, name: "SwopLbl", text:"Количество обменов: ", lablIndex:8, x:9, y:204);
+            
             // 
             // SelectionSortBtn
             // 
-            this.SelectionSortBtn.Location = new System.Drawing.Point(12, 134);
-            this.SelectionSortBtn.Name = "SelectionSortBtn";
-            this.SelectionSortBtn.Size = new System.Drawing.Size(75, 23);
-            this.SelectionSortBtn.TabIndex = 12;
-            this.SelectionSortBtn.Text = "SelectionSort";
-            this.SelectionSortBtn.UseVisualStyleBackColor = true;
-            this.SelectionSortBtn.Click += new System.EventHandler(this.SelectionSortBtn_Click);
+            CreateBtn(x: 12, y:134, btnName:"SelectionSortBtn", tabIndex:12, btnText:"SelectionSort", handler:this.SelectionSortBtn_Click, btn:this.SelectionSortBtn);
             
             // 
             // QuickSortBtn
             // 
-            this.QuickSortBtn.Location = new System.Drawing.Point(90, 134);
-            this.QuickSortBtn.Name = "QuickSortBtn";
-            this.QuickSortBtn.Size = new System.Drawing.Size(75, 23);
-            this.QuickSortBtn.TabIndex = 19;
-            this.QuickSortBtn.Text = "QuickSort";
-            this.QuickSortBtn.UseVisualStyleBackColor = true;
-            this.QuickSortBtn.Click += new System.EventHandler(this.QuickSortBtn_Click);
+            CreateBtn(x: 90, y:134, btnName:"QuickSortBtn", tabIndex:19, btnText:"QuickSort", handler:this.QuickSortBtn_Click, btn:this.QuickSortBtn);
+            
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(350, 242);
-            this.Controls.Add(this.QuickSortBtn);           
-            this.Controls.Add(this.SelectionSortBtn);  
+            this.Controls.Add(this.QuickSortBtn);
+            this.Controls.Add(this.SelectionSortBtn);
             this.Controls.Add(this.ClearBtn);
             this.Controls.Add(this.SwopLbl);
             this.Controls.Add(this.CompareLbl);
-            this.Controls.Add(this.TimeLbl);          
+            this.Controls.Add(this.TimeLbl);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -214,6 +165,43 @@
         }
 
         #endregion
+
+        private void CreateTextBox(TextBox textBox,string name)
+        {
+            textBox.Location = new System.Drawing.Point(15, 25);
+            textBox.Name = name;
+            textBox.Size = new System.Drawing.Size(228, 20);
+            textBox.TabIndex = 0;
+        }
+
+        private void CreatePanel(Panel panel, string name, int panelIndex, int x, int y, int width, int height)
+        {
+            panel.Location = new System.Drawing.Point(x, y);
+            panel.Name = name;
+            panel.Size = new System.Drawing.Size(width, height);
+            panel.TabIndex = panelIndex;
+        }
+
+        private void CreateBtn(Button btn, string btnName, int tabIndex, string btnText, EventHandler handler, int x, int y)
+        {
+            btn.Location = new System.Drawing.Point(x, y);
+            btn.Name = btnName;
+            btn.Size = new System.Drawing.Size(75, 23);
+            btn.TabIndex = tabIndex;
+            btn.Text = btnText;
+            btn.UseVisualStyleBackColor = true;
+            btn.Click += new System.EventHandler(handler);
+        }
+
+        private void CreateLbl(Label label, string text, string name, int x, int y, int lablIndex)
+        {
+            label.AutoSize = true;
+            label.Location = new System.Drawing.Point(x, y);
+            label.Name = name;
+            label.Size = new System.Drawing.Size(119, 13);
+            label.TabIndex = lablIndex;
+            label.Text = text;
+        }
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
